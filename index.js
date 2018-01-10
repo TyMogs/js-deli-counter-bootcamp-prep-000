28924cd7d7a37204c6c30646c00fcf856c87d413
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-10 23:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeli, name) {
 var katzDeliLine = []
 
 function currentLine(katzDeli) {
-  for (var i = 0; i < katzDeli.length; i++) {
+  for (let i = 0; i < katzDeli.length; i++) {
     katzDeliLine.push(` ${i+1}. ${katzDeli[i]}, `)
     if (katzDeli.length === 0) {
       return "The line is currently empty."
