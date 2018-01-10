725abb15144fa85f20ab48add3dfc178aac7f2d4
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-10 22:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeli = katzDeliLine.push(name);
   for (var i = 0; i < katzDeli; i++) {
-  return`welcome, ${name}. You are number ${katzDeli[i]} in line.`;
+  return`Welcome, ${name}. You are number ${katzDeli[i]} in line.`;
   }
 }
 
 function nowServing(katzDeli, name) {
   if (katzDeli.length > 0) {
-    return `Currently serving ${katzDeli[0]}`
+    return `Currently serving ${katzDeli[0]}.`
     katzDeli.shift(0)
   } else {
   return "There is nobody waiting to be served!"
