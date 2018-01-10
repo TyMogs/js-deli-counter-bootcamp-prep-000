74873e525afe23495e79c81f4b9ef916275b4707
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-10 21:55:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   for (var i = 0; i < katzDeliLine.length; i++)
   katzDeli = katzDeliLine.push(name);
-  return katzDeli[i+1];
+  return`welcome, ${name}. You are number ${katzDeli[i+1]} in line`;
 }
 
 function nowServing(name) {
